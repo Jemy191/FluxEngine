a167f2a8a7a5b18e6bd3a51a12379bc0098b17c4
Émilie Collard <emiliecollard191@outlook.com>
2024-04-16 03:55:45 -0400

    Move some thing in test app

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -19,20 +19,20 @@ List<AssetSource> assetSources =
 [
     new FileSystemAssetSource(catalogues.MaxBy(c => c.BuildVersion)!, "Assets")
 ];
-var assetService = new AssetsService(assetSources);
-assetService.RegisterImporter<JsonAsset>(new JsonImporter());
+var assetServices = new AssetsService(assetSources);
+assetServices.RegisterImporter<JsonAsset>(new JsonImporter());
 
-var fileSources = assetService.AssetSources.OfType<FileSystemAssetSource>();
+var cities = await assetServices.Load<JsonAsset>(new Guid("f053d056-8ec2-42c2-a4ed-350934ad9f2e"));
+
+Console.WriteLine(cities!.Root);
+
+var fileSources = assetServices.AssetSources.OfType<FileSystemAssetSource>();
 
 foreach (var fileSource in fileSources)
 {
     Console.WriteLine($"File source:");
     PrintAllFile(fileSource.assetTree);
 }
-
-var cities = await assetService.Load<JsonAsset>(new Guid("f053d056-8ec2-42c2-a4ed-350934ad9f2e"));
-
-//Console.WriteLine(cities!.Root);
 
 return;
 
