b837d900255edc2616c7af641c18af2c52d1453c
Émilie Collard <git@jemy191.ca>
2026-01-10 17:54:43 -0500

    Added a remark to IFileChangeWatcher.Flush

--- Flux.Resources/FileChangeWatchers/IFileChangeWatcher.cs
+++ Flux.Resources/FileChangeWatchers/IFileChangeWatcher.cs
@@ -5,8 +5,11 @@ public interface IFileChangeWatcher : IDisposable
     void StartWatchingFile(FileInfo file, Action onChanged);
     void StopWatchingFile(FileInfo file);
     /// <summary>
-    /// Call once per frame (on the main thread).
     /// Executes callbacks for files that have changed since the previous Flush.
     /// </summary>
+    /// <remarks>
+    /// Call once per frame (on the main thread).
+    /// If you plan to refresh a GraphicEngine object, ensure you do it before of after starting rendering.
+    /// </remarks>
     void Flush();
 }
